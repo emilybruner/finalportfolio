3f601e4aad356766df8a19ab5a144e7b1644d34e
Emily Bruner <bruner.emilym@gmail.com>
2020-08-21 03:02:45 -0400

    stuck on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,26 @@
 import React from 'react';
-
+import '../sass/Projects.scss'
+import gift from '../img/gift.jpg'
+import ladybug from '../img/ladybug.jpg'
+import pencils from '../img/pencils.jpg'
 
 export default function Projects() {
     return (
-        <h1>Featured Projects</h1>
+        <div>
+            <h4>Featured Projects</h4>
+            <div class='projects-container'>
+                <div class='project-box'>
+                <img src={gift} alt='gift box' class='project-pic'/>
+                </div>
+                <div class='project-box'>
+                    
+                </div>
+                <div class='project-box'>
+                    
+                </div>
+            
+            </div>    
+        </div>
+               
     )
 }
