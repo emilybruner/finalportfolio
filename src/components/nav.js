1b73b20898ef600cb9539f04ab045ab04d946a49
Emily Bruner <56037660+emilybruner@users.noreply.github.com>
2020-10-05 12:50:48 -0400

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
         <nav className="navigation">
             <ul><a href='#skills'>Skills</a></ul>
             <ul><a href='#footer'>Contact</a> </ul>
-            <ul><a href='https://drive.google.com/file/d/1IvcC4Bysav09i7Sl4Os39ki-WO0g4HTy/view?usp=sharing'>Resume</a> </ul>
+            <ul><a href='https://docs.google.com/document/d/1LVlgWo3LI19GV1zU-Mh4hbcOlIRL7U5hR5pY0rz4zak/edit?usp=sharing'>Resume</a> </ul>
             <ul ><a className='circle-button' href='#projects'>Projects</a> </ul>
         </nav>
     )
